1a52d9efbe82d379f67d4118c739126902bcd1ef
Lucas Costa Teotônio <69512117+Lucasdevs@users.noreply.github.com>
2020-08-18 10:12:38 -0300

    Add files via upload

---
+++ UsoCaneta.java
@@ -0,0 +1,21 @@
+package br.cesed.si.p2;
+
+public class UsoCaneta {
+	String marca;
+	String cor;
+	float ponta;
+	int carga;
+	boolean tampada;
+	
+	void rabiscar() {
+		
+	}
+	void tampar() {
+		
+	}
+		
+	void destampar() {
+		
+	}
+
+}
