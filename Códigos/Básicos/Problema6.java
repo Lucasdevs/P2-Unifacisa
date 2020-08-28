2468af7716aba271e3e738c83f7ee40158d21e4b
Lucas Costa Teotônio <69512117+Lucasdevs@users.noreply.github.com>
2020-08-28 12:12:25 -0300

    Create Problema6.java

---
+++ Códigos/Básicos/Problema6.java
@@ -0,0 +1,23 @@
+package br.cesed.si.p2.basicos;
+
+import java.util.Scanner;
+
+public class Problema6 {
+	public static void main (String[] args){
+		
+		Scanner entrada = new Scanner(System.in);
+		int cont = 0;
+		int cont2 = 0;
+		while(cont<5) {
+			int valor = entrada.nextInt();
+			int val= valor%3;
+			if(val == 0 ) {
+				cont2+=valor;
+			}
+			cont+=1;
+		}
+		System.out.println("Soma dos Múltiplos de Três: "+cont2);
+				
+		}
+
+	}
