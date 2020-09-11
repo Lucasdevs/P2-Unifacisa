957c9e6de0ce2e0035382ed551c9af714343aae8
Lucas Costa Teotônio <69512117+Lucasdevs@users.noreply.github.com>
2020-09-11 16:56:28 -0300

    Create Concessionaria.java

---
+++ Códigos/Classes e Objetos/Concessionaria.java
@@ -0,0 +1,24 @@
+package br.cesed.si.p2.carros;
+
+
+public class Concessionaria {
+	public static void main(String[] args) {
+		Carros palio1 = new Carros("Pálio","Fiat",35000);
+		Carros palio2 = new Carros("Pálio","Fiat","Prateado",35000,2,false,false,false,false);
+		Carros civic = new Carros("Civic","Honda","Desconhecida",110000,4,true,true,true,true);
+		Carros corolla = new Carros("Corolla","Toyota","Desconhecida",110000,4,true,true,false,true);
+		Carros gol = new Carros("Gol","Volkswagen","Desconhecida",55000,4,true,true,true,true);
+		
+		System.out.println(palio1);
+		System.out.println();
+		System.out.println(palio2);
+		System.out.println();
+		System.out.println(civic);
+		System.out.println();
+		System.out.println(corolla);
+		System.out.println();
+		System.out.println(gol);
+	
+	}
+		
+}
