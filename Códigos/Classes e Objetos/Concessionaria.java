3b83b971236ca757a333556b8de33c351a79533d
Lucas Costa Teotônio <69512117+Lucasdevs@users.noreply.github.com>
2020-09-11 17:37:38 -0300

    Update Concessionaria.java

--- Códigos/Classes e Objetos/Concessionaria.java
+++ Códigos/Classes e Objetos/Concessionaria.java
@@ -3,20 +3,16 @@ package br.cesed.si.p2.carros;
 
 public class Concessionaria {
 	public static void main(String[] args) {
-		Carros palio1 = new Carros("Pálio","Fiat",35000);
-		Carros palio2 = new Carros("Pálio","Fiat","Prateado",35000,2,false,false,false,false);
+		Carros palio1 = new Carros("PÃ¡lio","Fiat",35000);
+		Carros palio2 = new Carros("PÃ¡lio","Fiat","Prateado",35000,2,false,false,false,false);
 		Carros civic = new Carros("Civic","Honda","Desconhecida",110000,4,true,true,true,true);
 		Carros corolla = new Carros("Corolla","Toyota","Desconhecida",110000,4,true,true,false,true);
 		Carros gol = new Carros("Gol","Volkswagen","Desconhecida",55000,4,true,true,true,true);
 		
-		System.out.println(palio1);
-		System.out.println();
-		System.out.println(palio2);
-		System.out.println();
-		System.out.println(civic);
-		System.out.println();
-		System.out.println(corolla);
-		System.out.println();
+		System.out.println(palio1+"\n");
+		System.out.println(palio2+"\n");
+		System.out.println(civic+"\n");
+		System.out.println(corolla+"\n");
 		System.out.println(gol);
 	
 	}
